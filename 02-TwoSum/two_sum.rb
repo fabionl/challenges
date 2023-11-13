93492b413a8371d305c8968c6561b5aa0b3b9cf0
Fabio Lima <fabionl@gmail.com>
2023-11-13 11:52:17 -0800

    Fixes another bug in the logic hash logic

--- 02-TwoSum/two_sum.rb
+++ 02-TwoSum/two_sum.rb
@@ -4,23 +4,19 @@
 def two_sum(nums, target)
   compliments = {}
   last_idx = nums.count - 1
+  nums.each_with_index do |current, i|
+    compliments[current] = i
+  end
+
   for n in (0..last_idx) do
     next_idx = n + 1
-    base_num = nums[n]
+    current_num = nums[n]
 
-    compliment_idx = compliments[target - base_num]
-    if !compliment_idx.nil?
-      potential_num = nums[compliment_idx] + base_num
-      return [n, compliment_idx] if compliment_idx > n && (potential_num == target)
-    end
+    compliment_idx = compliments[target - current_num]
+    next if compliment_idx.nil?
 
-    for t in (next_idx..last_idx) do
-      current_num = nums[t]
-
-      return [n, t] if (base_num + current_num == target)
-
-      compliments[target - current_num] = t
-    end
+    potential_num = nums[compliment_idx] + current_num
+    return [n, compliment_idx] if compliment_idx > n && (potential_num == target)
   end
 
   return []
